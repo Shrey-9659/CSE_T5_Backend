1fca82108949ce17e2cca8a7156bcd7d792d5ae1
Shrey Khandelwal <shrey.khandelwal.2000@gmail.com>
2026-04-02 09:56:10 +0530

    File Serving Project - Lecture

--- P10/Express/server_01/index.js
+++ P10/Express/server_01/index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const path = require("path")
 const fs = require("fs")
+const users = require("./users.json")
 
 const homePage = path.join(__dirname,"/views/HomePage/index.html")
 const formPage = path.join(__dirname,"/views/RegistrationPage/index.html")
@@ -29,7 +30,12 @@ app.get("/home/submit", (req, res) => {
     res.redirect("/home/form")
 })
 app.post("/home/submit", (req, res) => {
+    // const newDataToAdd = {msg : "New data added"}
     const userData = req.body
+    const length = users.length
+    users.push({id : length+1, ...userData})
+    
+    fs.writeFileSync("users.json", JSON.stringify(users))
     fs.readFile(submitPage, "utf-8", (err, data) => {
         if(err) console.log(err)
             else {
@@ -46,3 +52,7 @@ app.post("/home/submit", (req, res) => {
 app.listen(3000, () => {
     console.log("Server started...")
 })
+
+// 1. user.json ka data index.js m chahiye, [{},{},{}]
+// 2. newData {}, isko userData m push karna hoga - [{},{},{},{new}]
+// 3. user.json ko update kar dena pdega
